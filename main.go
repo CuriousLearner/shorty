0df53684dd4c7168ba749b43dbae65080b42ebcd
Sanyam Khurana <sanyam.khurana01@gmail.com>
2018-12-04 16:37:01 +0530

    Dummy URL shortner

--- main.go
+++ main.go
@@ -1,72 +1,44 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
-	// "html"
 	"net/http"
-	// "regexp"
-	// "net/url"
-	b64 "encoding/base64"
-	"github.com/gorilla/mux"
+
+	"github.com/CuriousLearner/shorty/handlers"
 )
 
-func DecodeUrl(encoded_str string) (string, error) {
-	decoded_str, err := b64.StdEncoding.DecodeString(encoded_str)
-	return string(decoded_str), err
-}
-
-func EncodeUrl(string_url string) (string, error) {
-	return b64.StdEncoding.EncodeToString([]byte(string_url)), nil
-}
-
-func GetSHA1HashedURL(long_url string) []byte {
-	hash := sha1.New()
-	hash.Write([]byte(long_url))
-	return hash.Sum(nil)
-}
-
-func GetShortUrl(w http.ResponseWriter, r *http.Request) {
-	data := GetSHA1HashedURL(long_url)
-	short_url, err := EncodeUrl(fmt.Sprintf("%x", data))
-	if err != nil {
-		fmt.Printf("Could not convert url; error: %s", err)
-		return fmt.Sprintf("%s", err)
-	}
-	w.Write([]byte(short_url))
-}
-
-func GetLongUrl(w http.ResponseWriter, r *http.Request) {
-	long_url := "dummy"
-	w.Write([]byte(long_url))
-}
-
-func router(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	route_type := params["route_type"]
-
-	switch route_type {
-		case "encode":
-			GetShortUrl(w, r)
-		case "decode":
-			GetLongUrl(w, r)
-		default:
-			w.Write([]byte("Invalid choice."))
-	}
-}
-
 func main() {
-	CURRENT_HOST := "http://sh.rt"
-	r := mux.NewRouter()
-	http.HandleFunc("/", router)
-	// http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// 	fmt.Fprintf(w, "Hello, long_url: google.com, short_url: %q", CURRENT_HOST + "/" + html.EscapeString(GetShortUrl("google.com"))[:6])
-	// }))
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		fmt.Printf("Error")
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
+	mapHandler := handlers.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/final
+`
+	yamlHandler, err := handlers.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
 }
 
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
